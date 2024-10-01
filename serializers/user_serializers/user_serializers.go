c8f45f00f86a2cd2eff8c877b695288cc1cac8d0
saviganga <akatugbasaviour@gmail.com>
2024-10-01 09:43:08 +0100

    feat: cleanup - user serializers (admin)

--- serializers/user_serializers/user_serializers.go
+++ serializers/user_serializers/user_serializers.go
@@ -2,6 +2,7 @@ package user_serializers
 
 import (
 	"time"
+	"txrnxp/models"
 
 	"github.com/google/uuid"
 )
@@ -28,4 +29,22 @@ type ExportUserSerializer struct {
 	FirstName   string    `json:"first_name"`
 	LastName    string    `json:"last_name"`
 	PhoneNumber string    `json:"phone_number"`
+}
+
+func SerializeUser(user models.AdminUser) (UserSerializer) {
+
+	serialized_user := new(UserSerializer) 
+
+	serialized_user.Id = user.Id
+	serialized_user.Email = user.Email
+	serialized_user.UserName = user.UserName
+	serialized_user.FirstName = user.FirstName
+	serialized_user.LastName = user.LastName
+	serialized_user.PhoneNumber = user.PhoneNumber
+	serialized_user.IsActive = user.IsActive
+	serialized_user.LastLogin= user.LastLogin
+	serialized_user.CreatedAt = user.CreatedAt
+	serialized_user.UpdatedAt = user.UpdatedAt
+
+	return *serialized_user
 }
--- utils/admin_utils/admin.go
+++ utils/admin_utils/admin.go
@@ -11,11 +11,10 @@ import (
 	"github.com/golang-jwt/jwt"
 )
 
-func CreateAdminUser(c *fiber.Ctx) (*models.AdminUser, error) {
+func CreateAdminUser(c *fiber.Ctx) (*user_serializers.UserSerializer, error) {
 
 	db := initialisers.ConnectDb().Db
 	user := new(models.AdminUser)
-	serialized_user := new(user_serializers.UserSerializer) 
 	err := c.BodyParser(user)
 	if err != nil {
 		return nil, errors.New("invalid request body")
@@ -26,37 +25,20 @@ func CreateAdminUser(c *fiber.Ctx) (*models.AdminUser, error) {
 		return nil, errors.New(err.Error())
 	}
 
-	serialized_user.Id = user.Id
-	serialized_user.Email = user.Email
-	serialized_user.UserName = user.UserName
-	serialized_user.FirstName = user.FirstName
-	serialized_user.LastName = user.LastName
-	serialized_user.PhoneNumber = user.PhoneNumber
-	serialized_user.IsActive = user.IsActive
-	serialized_user.LastLogin= user.LastLogin
-	serialized_user.CreatedAt = user.CreatedAt
-	serialized_user.UpdatedAt = user.UpdatedAt
+	serialized_user := user_serializers.SerializeUser(*user)
 
-	return user, nil
+	return &serialized_user, nil
+
 }
 
 func GetAdminUsers(c *fiber.Ctx) error {
 	authenticated_user := c.Locals("user").(jwt.MapClaims)
 	db := initialisers.ConnectDb().Db
 	users := []models.AdminUser{}
-	serialized_user := new(user_serializers.UserSerializer) 
 	db.First(&users, "id = ?", authenticated_user["id"])
-	
-	serialized_user.Id = users[0].Id
-	serialized_user.Email = users[0].Email
-	serialized_user.UserName = users[0].UserName
-	serialized_user.FirstName = users[0].FirstName
-	serialized_user.LastName = users[0].LastName
-	serialized_user.PhoneNumber = users[0].PhoneNumber
-	serialized_user.IsActive = users[0].IsActive
-	serialized_user.LastLogin= users[0].LastLogin
-	serialized_user.CreatedAt = users[0].CreatedAt
-	serialized_user.UpdatedAt = users[0].UpdatedAt
-	
+
+	serialized_user := user_serializers.SerializeUser(users[0])
+
 	return utils.SuccessResponse(c, serialized_user, "Successfully fetched users")
+
 }
