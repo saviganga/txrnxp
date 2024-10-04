fae57d61b543dad9925a6f76b186ac71a759caeb
saviganga <akatugbasaviour@gmail.com>
2024-10-04 23:14:34 +0100

    feat: add barcode to user ticket - 2

--- initialisers/db.go
+++ initialisers/db.go
@@ -39,8 +39,8 @@ func ConnectDb() Dbinstance {
 	// log.Println("db connected")
 	db.Logger = logger.Default.LogMode(logger.Error)
 
-	log.Println("running migrations")
-	db.AutoMigrate(&models.Xuser{}, &models.UserWallet{}, &models.XuserAuthToken{}, &models.AdminUser{}, &models.AdminUserAuthToken{}, &models.Business{}, models.Event{}, models.EventTicket{}, models.UserTicket{}, models.TransactionEntries{})
+	// log.Println("running migrations")
+	// db.AutoMigrate(&models.Xuser{}, &models.UserWallet{}, &models.XuserAuthToken{}, &models.AdminUser{}, &models.AdminUserAuthToken{}, &models.Business{}, models.Event{}, models.EventTicket{}, models.UserTicket{}, models.TransactionEntries{})
 
 	DB := Dbinstance{
 		Db: db,
